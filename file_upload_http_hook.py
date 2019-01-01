c67c42a0a598c17ffd608818e3f9e53d2c7ab4fc
madumalt <madumalt@gmail.com>
2019-01-01 18:08:05 +0530

    adding all vars to airflow plugin

--- file_upload_http_hook.py
+++ file_upload_http_hook.py
@@ -85,3 +85,23 @@ class AirflowFileUploadHttpPlugin(AirflowPlugin):
     # A list of class(es) derived from BaseHook
     # Will show up under airflow.hooks.file_upload_http_plugin.FileUploadHttpHook
     hooks = [FileUploadHttpHook]
+
+    # A list of class(es) derived from BaseOperator
+    operators = []
+    # A list of class(es) derived from BaseSensorOperator
+    sensors = []
+    # A list of class(es) derived from BaseExecutor
+    executors = []
+    # A list of references to inject into the macros namespace
+    macros = []
+    # A list of objects created from a class derived
+    # from flask_admin.BaseView
+    admin_views = []
+    # A list of Blueprint object created from flask.Blueprint. For use with the flask_admin based GUI
+    flask_blueprints = []
+    # A list of menu links (flask_admin.base.MenuLink). For use with the flask_admin based GUI
+    menu_links = []
+    # A list of dictionaries containing FlaskAppBuilder BaseView object and some metadata. See example below
+    appbuilder_views = []
+    # A list of dictionaries containing FlaskAppBuilder BaseView object and some metadata. See example below
+    appbuilder_menu_items = []
